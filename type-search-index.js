1481d346dd0ac979d83558022ac5d59e6422c02f
atJustinG <45684351+atJustinG@users.noreply.github.com>
2024-04-14 18:13:33 +0200

    Javadoc added

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"bruch","l":"Bruch"},{"p":"bruch","l":"BruchComparator"},{"p":"bruch","l":"bruchTest"}];updateSearchResults();
